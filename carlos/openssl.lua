e6c87561edf2c1b3f736940a7a612eb8b18594b9
Carlos <carlos@asus.my.domain>
2017-11-26 18:27:05 -0600

    New algos from ald ones

--- carlos/openssl.lua
+++ carlos/openssl.lua
@@ -15,10 +15,24 @@ _ENV = nil
 -- Public function definitions --
 ---------------------------------
 
+function M.md2( msg ) return dgs.digest('md2', msg) end
+
 function M.md5( msg ) return dgs.digest('md5', msg) end
+
+function M.md5sha1( msg) return dgs.digest('md5_sha1', msg) end
 
 function M.sha1( msg ) return dgs.digest('sha1', msg) end
 
-function M.sha256( msg ) return dgs.digest('sha256', msg) end
+function M.sha224( msg ) return dgs.digest('sha224', msg) end
+
+function M.sha384( msg ) return dgs.digest('sha384', msg) end
+
+function M.sha512( msg ) return dgs.digest('sha512', msg) end
+
+function M.dss( msg ) return dgs.digest('dss', msg) end
+
+function M.dss1( msg ) return dgs.digest('dss1', msg) end
+
+function M.ripemd160( msg ) return dgs.digest('ripemd160', msg) end
 
 return M 
