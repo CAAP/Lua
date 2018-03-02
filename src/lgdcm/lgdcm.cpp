03262d2ef58192f9f47ae98985b12a765dc5cb12
Carlos Aguilar <carlos@ptct.neuro>
2018-03-01 19:34:49 -0600

    dsd
    sdf
    jkgdef
    ds

--- src/lgdcm/lgdcm.cpp
+++ src/lgdcm/lgdcm.cpp
@@ -1,12 +1,12 @@
-#include <gdcm-2.9/gdcmImageReader.h>
-#include <gdcm-2.9/gdcmImage.h>
-#include <gdcm-2.9/gdcmGlobal.h>
-#include <gdcm-2.9/gdcmDicts.h>
-#include <gdcm-2.9/gdcmStringFilter.h>
-#include <gdcm-2.9/gdcmVR.h>
-#include <gdcm-2.9/gdcmScanner.h>
-#include <gdcm-2.9/gdcmDefs.h>
-#include <gdcm-2.9/gdcmWriter.h>
+#include <gdcmImageReader.h>
+#include <gdcmImage.h>
+#include <gdcmGlobal.h>
+#include <gdcmDicts.h>
+#include <gdcmStringFilter.h>
+#include <gdcmVR.h>
+#include <gdcmScanner.h>
+#include <gdcmDefs.h>
+#include <gdcmWriter.h>
 
 #include <lua.hpp>
 #include <lauxlib.h>
@@ -18,6 +18,8 @@ extern "C" {
 #include "anon.h"
 
 #define checkimage(L,i) (char *)luaL_checkudata(L, i, "caap.gdcm.image")
+
+// AUXILIAR FNs
 
 typedef struct gdcmtags {
     int size;
@@ -77,7 +79,9 @@ static int initializeXML() {
     return 0;
 }
 
-// PUBLIC FUNCTIONS
+//////////////////////// PUBLIC FUNCTIONS //////////////////////////
+
+// VALID TAG
 
 static int isValidTag(lua_State *L) {
     luaL_checktype(L, 1, LUA_TTABLE);
@@ -90,6 +94,7 @@ static int isValidTag(lua_State *L) {
     lua_pushboolean(L, valid);
     return 1;
 }
+
 
 // IMAGE
 
@@ -137,37 +142,11 @@ static int imageReader(lua_State *L) {
     return 1; // returns userdatum: caap.gdcm.image
 }
 
-static int dimx(lua_State *L) {
-    lua_getuservalue(L, 1);
-    lua_getfield(L, -1, "dimX");
-    return 1;
-}
+//-----------------------------------------//
+// READERs read one file at a time  ;-O    //
+//-----------------------------------------//
 
-static int dimy(lua_State *L) {
-    lua_getuservalue(L, 1);
-    lua_getfield(L, -1, "dimY");
-    return 1;
-}
-
-static int intercept(lua_State *L) {
-    lua_getuservalue(L, 1);
-    lua_getfield(L, -1, "intercept");
-    return 1;
-}
-
-static int img2string(lua_State *L) {
-    lua_getuservalue(L, 1);
-    lua_getfield(L, -1, "asstring");
-    return 1;
-}
-
-static int img_len(lua_State *L) {
-    lua_getuservalue(L, 1);
-    lua_getfield(L, -1, "length");
-    return 1;
-}
-
-//READER
+// READER - confidential Profile | specialized version
 
 static int confidentialProfile(lua_State *L) {
     unsigned int N = nAttributes(); // number of dicom tags
@@ -181,6 +160,8 @@ static int confidentialProfile(lua_State *L) {
 
     return 1;
 }
+
+// READER
 
 static int newReader(lua_State *L) {
     luaL_checktype(L, 1, LUA_TTABLE); // table containing dicom tags
@@ -200,71 +181,6 @@ static int newReader(lua_State *L) {
 	*ptag++ = gdcm::Tag(j[0], j[1]);
     }
 
-    return 1;
-}
-
-static int readData(lua_State *L) {
-    Tags *ptags = (Tags *)luaL_checkudata(L, 1, "caap.gdcm.reader");
-    const char *fname = luaL_checkstring(L, 2);
-
-    unsigned int N = ptags->size;
-    gdcm::Reader reader;
-    reader.SetFileName( fname );
-    if ( reader.ReadUpToTag( ptags->tags[N-1] ) ) {
-	const gdcm::DataSet & ds = reader.GetFile().GetDataSet();
-	const gdcm::FileMetaInformation & header = reader.GetFile().GetHeader();
-	gdcm::StringFilter sf;
-	sf.SetFile( reader.GetFile() );
-        // create result TABLE
-	lua_newtable(L);
-	lua_pushstring(L, fname); // add filename
-	lua_rawseti(L, -2, 1);
-	gdcm::Tag *ptag = ptags->tags;
-	unsigned int k, w = 0;
-	for (k=0; k < N;) {
-	    gdcm::Tag tag( ptag[k++] );
-	    std::string s = "";
-	    if ( tag.GetGroup() == 0x2 && header.FindDataElement( tag ))
-		s = sf.ToString( header.GetDataElement( tag ).GetTag() );
-	    else if ( ds.FindDataElement( tag ) )
-		s = sf.ToString( ds.GetDataElement( tag ).GetTag() );
-	    else
-		w++; // missing value
-	    // ADD value/empty string
-	    if (!lua_stringtonumber(L, s.c_str()))
-		lua_pushstring(L, s.c_str());
-/*	    lua_pushstring(L, s.c_str());
-	    if ( lua_isnumber(L, -1) == 1 ) {
-		lua_pushnumber(L, lua_tonumber(L, -1));
-		lua_remove(L, -2);
-	    } */
-	    lua_rawseti(L, -2, k+1);
-	}
-// mising values could be added to TABLE
-        return 1;
-    }
-    else { lua_pushnil(L); lua_pushstring(L, "Error reading file."); return 2; }
-}
-
-static int isValid(lua_State *L) {
-    Tags *ptags = (Tags *)luaL_checkudata(L, 1, "caap.gdcm.reader");
-    const char *fname = luaL_checkstring(L, 2);
-
-    int N = ptags->size;
-    gdcm::Reader reader;
-    reader.SetFileName( fname );
-    lua_pushboolean( L, reader.ReadUpToTag( ptags->tags[N-1] ) );
-    return 1;
-}
-
-static int rdr_len(lua_State *L) {
-    Tags *ptags = (Tags *)luaL_checkudata(L, 1, "caap.gdcm.reader");
-    lua_pushinteger( L, ptags->size );
-    return 1;
-}
-
-static int rdr2string(lua_State *L) {
-    lua_pushstring(L, "GDCM-Reader");
     return 1;
 }
 
@@ -312,6 +228,9 @@ static int deidentify( lua_State *L ) {
     return 1;
 }
 
+//---------------------------------------------//
+//    Read a directory using the given tags    //
+//---------------------------------------------//
 // SCANNER
 
 static int scanner(lua_State *L) {
@@ -321,7 +240,7 @@ static int scanner(lua_State *L) {
     luaL_checktype(L, 2, LUA_TTABLE); // table containing dicom tags
     bool recurse = (1 == lua_toboolean(L, 3)); // whether recursion is performed
 
-    d.Load( dirname, recurse ); // Load files from directory; recursively
+    d.Load( dirname, recurse ); // Load files from directory (recursively)
 
     int k, N = luaL_len(L, 2); // N is the number of tags
     unsigned int j[2];
@@ -336,7 +255,7 @@ static int scanner(lua_State *L) {
 
     if (!scn.Scan( d.GetFilenames() )) {lua_pushnil(L); lua_pushfstring(L, "Unable to scan directory: %s.\n", dirname); return 2;}
 
-    gdcm::Directory::FilenamesType files = scn.GetKeys(); // it is a vector structure
+    gdcm::Directory::FilenamesType files = scn.GetKeys(); // it is a vector structure containing only DICOM files
 
     if ( !files.empty() ) {
 	    lua_newtable(L);
@@ -347,7 +266,7 @@ static int scanner(lua_State *L) {
 		    lua_pushstring(L, fname);
 		    lua_rawseti(L, -2, 1);
 		    for (k=0; k<N; k++) {
-			    const char *value = scn.GetValue(fname, tags[k]); // maybe change to Mappings as suggested in Doc!
+			    const char *value = scn.GetValue(fname, tags[k]); // maybe change to Mapping as suggested in Docs:   Examples/Cxx/SimpleScanner.cxx
 			    if(!lua_stringtonumber(L, value))
 				lua_pushstring(L, (value ? value : ""));
 /*			    lua_pushstring(L, scn.GetValue(fname, tags[k]));
@@ -367,6 +286,107 @@ static int scanner(lua_State *L) {
 	    {lua_pushnil(L); lua_pushfstring(L, "Unable to find DCM files in directory: %s.\n", dirname); return 2;}; //luaL_error(L, "Unable to read directory.");
     return 1;
 }
+
+
+//////////////////  Interface METHODs  //////////////
+
+// GDCM-Image
+
+static int dimx(lua_State *L) {
+    lua_getuservalue(L, 1);
+    lua_getfield(L, -1, "dimX");
+    return 1;
+}
+
+static int dimy(lua_State *L) {
+    lua_getuservalue(L, 1);
+    lua_getfield(L, -1, "dimY");
+    return 1;
+}
+
+static int intercept(lua_State *L) {
+    lua_getuservalue(L, 1);
+    lua_getfield(L, -1, "intercept");
+    return 1;
+}
+
+static int img2string(lua_State *L) {
+    lua_getuservalue(L, 1);
+    lua_getfield(L, -1, "asstring");
+    return 1;
+}
+
+static int img_len(lua_State *L) {
+    lua_getuservalue(L, 1);
+    lua_getfield(L, -1, "length");
+    return 1;
+}
+
+// GDCM-Reader
+
+static int readData(lua_State *L) {
+    Tags *ptags = (Tags *)luaL_checkudata(L, 1, "caap.gdcm.reader");
+    const char *fname = luaL_checkstring(L, 2);
+
+    unsigned int N = ptags->size;
+    gdcm::Reader reader;
+    reader.SetFileName( fname );
+    if ( reader.ReadUpToTag( ptags->tags[N-1] ) ) {
+	const gdcm::DataSet & ds = reader.GetFile().GetDataSet();
+	const gdcm::FileMetaInformation & header = reader.GetFile().GetHeader();
+	gdcm::StringFilter sf;
+	sf.SetFile( reader.GetFile() );
+        // create result TABLE
+	lua_newtable(L);
+	lua_pushstring(L, fname); // add filename
+	lua_rawseti(L, -2, 1);
+	gdcm::Tag *ptag = ptags->tags;
+	unsigned int k, w = 0;
+	for (k=0; k < N;) {
+	    gdcm::Tag tag( ptag[k++] );
+	    std::string s = "";
+	    if ( tag.GetGroup() == 0x2 && header.FindDataElement( tag ))
+		s = sf.ToString( header.GetDataElement( tag ).GetTag() );
+	    else if ( ds.FindDataElement( tag ) )
+		s = sf.ToString( ds.GetDataElement( tag ).GetTag() );
+	    else
+		w++; // missing value
+	    if (!lua_stringtonumber(L, s.c_str())) // ADD value/empty string
+		lua_pushstring(L, s.c_str());
+	    lua_rawseti(L, -2, k+1);
+	}
+	lua_pushinteger(L, w);
+	lua_setfield(L, -2, "missed"); // mising values are added to TABLE
+        return 1;
+    }
+    else { lua_pushnil(L); lua_pushstring(L, "Error reading file."); return 2; }
+}
+
+static int isValid(lua_State *L) {
+    Tags *ptags = (Tags *)luaL_checkudata(L, 1, "caap.gdcm.reader");
+    const char *fname = luaL_checkstring(L, 2);
+
+    int N = ptags->size;
+    gdcm::Reader reader;
+    reader.SetFileName( fname );
+    lua_pushboolean( L, reader.ReadUpToTag( ptags->tags[N-1] ) );
+    return 1;
+}
+
+static int rdr_len(lua_State *L) {
+    Tags *ptags = (Tags *)luaL_checkudata(L, 1, "caap.gdcm.reader");
+    lua_pushinteger( L, ptags->size );
+    return 1;
+}
+
+static int rdr2string(lua_State *L) {
+    lua_pushstring(L, "GDCM-Reader");
+    return 1;
+}
+
+
+
+//////////////////////////////////////////////////
 
 static const struct luaL_Reg dcm_funcs[] = {
     {"open", imageReader},
@@ -416,47 +436,4 @@ int luaopen_lgdcm (lua_State *L) {
 #ifdef __cplusplus
 }
 #endif
-
-/*
-static int IODName(lua_State *L) {
-    const char *filename = luaL_checkstring(L, 1);
-
-    gdcm::Reader reader;
-    reader.SetFileName( filename );
-    if ( !reader.Read() ) {
-	    lua_pushnil(L); lua_pushfstring(L, "Could not read: %s", filename);
-	    return 2;
-    }
-    gdcm::File &file = reader.GetFile();
-
-    if ( !initializeXML() ) {
-	lua_pushboolean(L, 0);
-	lua_pushfstring(L, "Error loading GDCM Resources path.\n");
-	return 2;
-    }
-
-    gdcm::MediaStorage ms;
-    ms.SetFromFile( file );
-    const gdcm::IODs &iods = defs.GetIODs();
-    const gdcm::Modules &modules = defs.GetModules();
-    const gdcm::Macros &macros = defs.GetMacros();
-    const char *iodname = gdcm::Defs::GetIODNameFromMediaStorage( ms );
-    if( !iodname ) { lua_pushnil(L); return 1; }
-    const gdcm::IOD &iod = iods.GetIOD( iodname );
-
-    gdcm::IOD::SizeType niods = iod.GetNumberOfIODs();
-    bool v = true;
-    unsigned int idx;
-    for (idx = 0; idx < niods; idx++) {
-	const gdcm::IODEntry &iodentry = iod.GetIODEntry(idx);
-	const char *ref = iodentry.GetRef();
-	gdcm::Usage::UsageType ut = iodentry.GetUsageType();
-	const gdcm::Module &module = modules.GetModule( ref );
-	v = v && module.Verify( file.GetDataSet(), ut );
-    }
-    lua_pushboolean(L, v);
-
-    return 1;
-}
-*/
 
