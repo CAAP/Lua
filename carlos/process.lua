26db44901cbdacc85564dd142ad4667486a2dd6b
Carlos <capagp@gmail.com>
2018-09-25 19:32:05 -0500

    sdf584sdf

--- carlos/process.lua
+++ carlos/process.lua
@@ -1,7 +1,11 @@
 local ocv = require'locv'
 local fd = require'carlos.fold'
 
-local PATH='dif01.mp4'
+local PATH='fesormex.mp4'
+
+local RECT = ocv.rect{x=0, y=0, width=1920, height=500}
+
+local M = {RECT=RECT}
 
 -- FNs --
 
@@ -9,13 +13,15 @@ local function inrange(c)
     local a = c:property'Area'
     if a > 1000 and a < 1e5 and c:property'S' > 0.9 then
 	local d = c:property('E'):dims()
-	return ((1.0 * d.width) / d.height) > 0.9
+	return ((1.0 * d.width) / d.height) > 0.85
     end
 end
 
+local function getROI( frame ) return RECT:apply(frame) end
+
 local function preprocess( frame ) return frame:togray(1):tobyte():median(11) end
 
-local function darkest( frame ) return frame:copy( frame:range(0,80) ) end
+local function darkest( frame ) return frame:copy( frame:range(0,95) ) end
 
 local function iseye( frame ) return fd.first( frame:contours'CC', inrange ) end
 
@@ -25,6 +31,16 @@ local function drawEllipse( econtour, frame, thick ) econtour:draw(frame, thick)
 
 local function workflow( x ) return fd.apply(x, preprocess, darkest, iseye, ellipse) end
 
+--[[
+M.inrange = inrange
+M.getROI = getROI
+M.preprocess = preprocess
+M.darkest = darkest
+
+return M
+--]]
+
+
 ----- Vars -----
 
 local movit = function() return ocv.openVideo(PATH),nil,nil end
@@ -32,19 +48,28 @@ local movit = function() return ocv.openVideo(PATH),nil,nil end
 ---------------
 
 -- Iterate until first eye-frame is found!
-local frameOne, initTime = fd.first(movit, function(x) return fd.apply(x, preprocess, darkest, iseye) end)
+local frameOne, initTime = fd.first(movit, function(x) return fd.apply(x, getROI, preprocess, darkest, iseye) end)
+
+frameOne = RECT:apply(frameOne)
 
 print(frameOne, initTime)
 
-local ellipseOne = workflow(frameOne)
+-------------------
 
-print(ellipseOne)
+frameOne:save'frameOne.png'
 
-drawEllipse(ellipseOne, frameOne, 3)
+local contours = fd.apply(frameOne, preprocess, darkest, function(fr) return fr:contours'CC' end)
 
-local it = movit()
+local ellipses = fd.reduce(contours, fd.filter(inrange), fd.map(ellipse), fd.into, {})
 
-for k=1,10 do
-    local tm, fr = it()
-    drawEllipse(ellipseOne, fr, 2)
-end
+local frameTwo = frameOne
+
+fd.reduce(ellipses, function(e) frameTwo = e:draw(frameTwo, 3) end)
+
+frameTwo:save'ellipses.png'
+
+-------------------
+
+--]]
+
+
