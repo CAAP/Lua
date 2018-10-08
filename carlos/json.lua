3a9050dc597c743690272ce54e19ff5f35026473
SmartGoggles <sg@medaid02.medaid.app>
2018-10-08 13:58:50 -0500

    sdf34sadf

---
+++ carlos/json.lua
@@ -0,0 +1,52 @@
+-- module setup
+local M = {}
+
+-- Import Section
+
+local concat = table.concat
+local format = string.format
+local open = io.open
+local asint = math.tointeger
+local assert = assert
+local pairs = pairs
+local type = type
+local tonumber = tonumber
+
+-- No more external access after this point
+_ENV = nil -- or M
+
+-- Local Variables for module-only access
+
+--------------------------------
+-- Local function definitions --
+--------------------------------
+
+local function asJSON()
+    local ret = {}
+    for k,v in pairs(w) do
+	local u = type(v) == 'table' and asJSON(v) or (asint(v) or v)
+	ret[#ret+1] = format('%q: %'..(tonumber(u) and 's' or 'q'), k, u)
+    end
+    return format('{%s}', concat(ret, ', ')):gsub('"%[', '['):gsub(']"', ']'):gsub("'", '"')
+end
+
+---------------------------------
+-- Public function definitions --
+---------------------------------
+
+M.asJSON = asJSON
+
+function M.sql2json( q )
+    assert(open(q.dbname)) -- file MUST exists
+    local db = assert(sql.connect(q.dbname), 'Could not connect to DB '..q.dbname)
+    if db.count(q.tbname. q.clause) > 0 then
+	local ret = fd.reduce( db.query( q.QRY ), fd.map( asJSON ), fd.into, {} )
+	return format('Content-Type: text/plain; charset=utf-8\r\n\r\n[%s]\n', concat(ret, ', '))
+    else
+	return 'Content-Type: text/plain\r\n\r\n[]\n'
+    end
+end
+
+
+return M
+
