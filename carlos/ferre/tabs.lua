7c6161858ea30f7e87f98903fb0e95e08554cead
SmartGoggles <sg@medaid02.medaid.app>
2019-10-23 14:40:07 -0500

    kjsd0f

---
+++ carlos/ferre/tabs.lua
@@ -0,0 +1,65 @@
+
+-- Import Section
+--
+local fd	= require'carlos.fold'
+
+local cache	= require'carlos.ferre'.cache
+
+local format	= require'string'.format
+
+local print	= print
+
+-- No more external access after this point
+_ENV = nil -- or M
+
+-- Local Variables for module-only access
+--
+local PINS	 = cache'Hi PINS'
+
+local PIDS	 = {}
+local FRUITS	 = {}
+
+--------------------------------
+-- Local function definitions --
+--------------------------------
+--
+ -- cmds: tabs, delete, msgs
+local function update(pid, cmd, msg)
+    if not PIDS[pid] then PIDS[pid] = {} end
+    local p = PIDS[pid]
+    if cmd == 'delete' and p.tabs then p.tabs = nil
+    else p[cmd] = '%s ' .. msg end -- add fruit
+end
+
+local function switch( msg, msgr )
+
+    local cmd = msg:match'%a+'
+    if cmd == 'CACHE' then
+	local fruit = msg:match'%s(%a+)'
+	PINS.sndkch( msgr, fruit )
+	print('CACHE sent to', fruit, '\n')
+	goto FIN
+    end
+    local pid = msg:match'pid=(%d+)'
+    if cmd == 'pins' then
+	PINS.store(pid, msg)
+	msgr:send_msg( msg )
+    elseif cmd == 'login' then
+	local fruit = msg:match'fruit=(%a+)'
+	local ft = FRUITS[pid]
+	if ft and ft ~= fruit then msgr:send_msg(format('%s logout pid=%d', ft, pid)) end
+	FRUITS[pid] = fruit
+	if PIDS[pid] then
+	    fd.reduce(fd.keys(PIDS[pid]), function(m) msgr:send_msg(format(m, fruit)) end)
+	end
+    else -- tabs, delete, msgs
+	update(pid, cmd, msg)
+	local ft = FRUITS[pid]
+	if cmd == 'msgs' and ft then msgr:send_msg(format('%s %s', ft, msg)) end
+    end
+    print(msg, '\n')
+    ::FIN::
+
+end
+
+return switch
