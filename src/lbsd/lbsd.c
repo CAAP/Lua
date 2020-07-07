2b0a7f7a7fcc4b8e1c57bd4c986df2d4602ef5f5
carlos <carlos@puff.petct.innn>
2020-07-07 11:45:18 -0500

    1111

--- src/lbsd/lbsd.c
+++ src/lbsd/lbsd.c
@@ -4,41 +4,6 @@
 #include <errno.h>
 #include <string.h>
 
-/*
-    int unveil(const char *path, const char *flags);
-
-static int unveil_raw(lua_State *L) {
-    const char *path = luaL_checkstring(L, 1);
-    const char *flags = luaL_checkstring(L, 2);
-
-    lua_pushboolean(L, 1);
-    if(0 == unveil(path, flags))
-	return 1;
-    else {
-	lua_pushnil(L);
-	lua_pushstring(L, strerror(errno));
-	return 2;
-    }
-}
-
-
-    unsigned int sleep(unsigned int);
-
-static int sleep_raw(lua_State *L) {
-    const unsigned int secs = luaL_checkinteger(L, 1);
-
-    lua_pushboolean(L, 1);
-    if(0 == sleep(secs))
-	return 1;
-    else {
-	lua_pushnil(L);
-	lua_pushstring(L, strerror(errno));
-	return 2;
-    }
-}
-
-    int pledge(const char *promises, const char *execpromises);
-*/
 
 // -1 means error, 0, 1, ... means success of some kind
 static int does_file_exists(lua_State *L) {
@@ -47,9 +12,9 @@ static int does_file_exists(lua_State *L) {
     return 1;
 }
 
+/* ***************** */
+
 static const struct luaL_Reg bsd_funcs[] = {
-//    {"unveil", unveil_raw},
-//    {"sleep", sleep_raw},
     {"file_exists", does_file_exists},
     {NULL, NULL}
 };
@@ -57,5 +22,6 @@ static const struct luaL_Reg bsd_funcs[] = {
 int luaopen_lbsd (lua_State *L) {
     // create library
     luaL_newlib(L, bsd_funcs);
+
     return 1;
 }
--- src/lmg/lmg.c
+++ src/lmg/lmg.c
@@ -32,31 +32,17 @@ MG_F_USER_6
 
 */
 
-
-static int mgr_gc(lua_State *L) {
-    struct mg_mgr *mgr = checkmgr(L);
-    if (mgr != NULL) {
-	mg_mgr_free(mgr);
-	mgr = NULL;
-    }
-    return 0;
-}
-
-static mgr_bind_http(lua_State *L) {
-    struct mg_mgr *mgr = checkmgr(L);
-    const char *port = luaL_checkstring(L, 2);
-    ;
-    struct mg_connection *c = mg_bind(mgr, port, handler);
-    mg_set_protocol_http_websocket(c);
-}
-
-static int mgr_poll(lua_State *L) {
-    struct mg_mgr *mgr = checkmgr(L);
-    int milis = lua_checkinteger(L, 2);
-    return mg_mgr_poll(mgr, milis);
-}
-
-
+// HELPERS
+// 
+// struct mg_str {
+// 	const char *p;
+// 	size_t len;
+// }
+//
+// struct mg_str mg_mk_str(const char *s);
+// struct mg_str mg_mk_str_n(const char *s, size_t len);
+//
+//
 
 
 /*
@@ -111,31 +97,133 @@ static send_error(lua_State *L) {
     return 1;
 }
 
+static void save_handler(lua_State *L, struct mg_connection *c, lua_CFunction f) {
+    // get table of handlers
+    lua_pushlightuservalue(L, &MGR);
+    lua_rawget(L, LUA_REGISTRYINDEX);
+    // store handler function
+    lua_pushlightuservalue(L, &c);
+    lua_pushcfunction(L, f);
+    lua_rawset(L, -3);
+    lua_pop(L, 1); // pop table of handlers
+}
+
+
+// EVENTS
+//
+// MG_EV_POLL
+// MG_EV_ACCEPT		socket_address
+// MG_EV_CONNECT	int
+// MG_EV_RECV		int *num_bytes
+// MG_EV_SEND		int *num_bytes
+// MG_EV_CLOSE
+// MG_EV_TIMER		double
+//
+// MG_EV_HTTP_REQUEST	struct http_message
+// MG_EV_HTTP_REPLY			"
+// MG_EV_HTTP_CHUNK			"
+// MG_EV_WEBSOCKET_HANDSHAKE_REQUEST	"
+// MG_EV_WEBSOCKET_HANDSHAKE_DONE	"
+// 			
+// MG_EV_WEBSOCKET_FRAME	struct websocket_message
+
+static void ev_handler(struct mg_connection *c, int ev, void *ev_data) {
+    lua_CFunction f = (lua_CFunction)c->user_data;
+    if (f) {
+	lua_State *L = (lua_State *)MGR->user_data;
+	// get handler from table XXX
+	lua_pushlightuserdata(L, ev_data);
+	f(L);
+    }
+}
+
+static int http_handler(lua_State *L) {
+    void *ev_data = lua_touserdata(L, -1);
+    lua_pop(l, 1); // pop lightuserdata
+    struct http_message *m = (struct http_message *)ev_data;
+    struct mg_str *ss = &m->method;
+    lua_pushlstring(L, ss->p, ss->len);
+    ss = &m->uri;
+    lua_pushlstring(L, ss->p, ss->len);
+    ss = &m->query_string;
+    lua_pushlstring(L, ss->p, ss->len);
+    ss = &m->body;
+    if (ss->len > 0)
+	lua_pushlstring(L, ss->p, ss->len);
+    else
+	lua_pushliteral(L, "");
+
+
+
+    return 0;
+}
+
+static mgr_bind_http(lua_State *L) {
+    const char *host = luaL_checkstring(L, 1);
+
+    struct mg_bind_opts bind_opts;
+    const char *err;
+    memset(&bind_opts, 0, sizeof(bind_opts));
+    bind_opts.error_string = &err;
+    // create a closure to save handler function as upvalue XXX
+    bind_opts.user_data = (void *)&http_handler;
+
+    struct mg_connection *c = mg_bind_opt(MGR, host, ev_handler, bind_opts);
+    if (c == NULL) {
+	lua_pushnil(L);
+	lua_pushfstring("Error: failed to create listener on host %s\n ", host);
+	return 2;
+    }
+    mg_set_protocol_http_websocket(c);
+    lua_pushboolean(L, 1);
+    return 1;
+}
+
+static int mgr_poll(lua_State *L) {
+    int mills = lua_checkinteger(L, 1);
+    return mg_mgr_poll(MGR, mills);
+}
+
+static int mgr_asstr(lua_State *L) {
+    lua_pushliteral(L, "Mongoose Event Manager (active)");
+    return 1;
+}
+
+static int mgr_gc(lua_State *L) {
+    if (MGR != NULL) {
+	mg_mgr_free(MGR);
+	MGR = NULL;
+    }
+    return 0;
+}
+
+/*   ******************************   */
 
 static void lmg_mgr(lua_State *L) {
     // create a manager
     struct mg_mgr *mgr = (struct mg_mgr *)lua_newuserdata(L, sizeof(struct mg_mgr));
     //
-    MRG = mgr;
+    MGR = mgr;
     // set its metatable
     luaL_getmetatable(L, "caap.mg.mgr");
     lua_setmetatable(L, -2);
-    // create the mongoose mgr
+    // create the mongoose mgr, keeping lua_State as userdata
     mg_mgr_init(mgr, L);
-    // add user value to mg_mgr userdata
+    // table to store handlers
     lua_newtable(L);
-    lua_setuservalue(L, -2);
-    // ;
-    lua_pushlightuserdata(L, &MRG);
-    lua_pushvalue(L, -2);
-    lua_rawset(L, LUA_REGISTRYINDEX);
+    lua_setservalue(L, -1);
 }
 
+/*   ******************************   */
+
+static const struct luaL_Reg mgr_meths[] = {
+    {"__tostring", mgr_asstr},
+    {"__gc",	   mgr_gc},
+    {NULL,	   NULL}
+};
 
 /*   ******************************   */
 
-
-/*   ******************************   */
 int luaopen_lmg (lua_State *L) {
     luaL_newmetatable(L, "caap.mg.mgr");
     lua_pushvalue(L, -1);
